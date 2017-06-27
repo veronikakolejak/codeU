93b6cff6599ad1ca37db36acb0ae4efef1a6cfcd
Veronika Kolejakova <veronika.kolejak@gmail.com>
2017-06-26 22:42:56 -0400

    Use only one 2dimensional array to remember state

--- Assignments/CountIslands/CountIslands.java
+++ Assignments/CountIslands/CountIslands.java
@@ -6,14 +6,13 @@ import java.util.Queue;
 public class CountIslands {
 
   public static int islandsCount(int width, int height, boolean[][] map) {
-    boolean[][] visitedTiles = new boolean[width][height];
     int count = 0;
 
     for (int y = 0; y < height; y++) {
       for (int x = 0; x < width; x++) {
-        // unvisited land tile found
-        if (!visitedTiles[x][y] && map[x][y]) {
-          bfs(x, y, map, visitedTiles);
+        // Unvisited land tile found
+        if (map[x][y]) {
+          bfs(x, y, map);
           count++;
         }
       }
@@ -22,7 +21,7 @@ public class CountIslands {
     return count;
   }
 
-  public static void bfs(int x, int y, boolean[][] map, boolean[][] visitedTiles) {
+  public static void bfs(int x, int y, boolean[][] map) {
     Queue<Integer> q = new LinkedList<>();
 
     q.add(x);
@@ -35,18 +34,15 @@ public class CountIslands {
       int curX = q.poll();
       int curY = q.poll();
 
-      visitedTiles[curX][curY] = true;
+      // Set the tile to be water as it shouldn't be visited again
+      map[curX][curY] = false;
 
       for (int i = 0; i < deltaX.length; i++) {
         int newX = curX + deltaX[i];
         int newY = curY + deltaY[i];
 
-        // Check if the new coordinates are in the correct range
-        if (newX < 0 || newX >= map.length || newY < 0 || newY >= map[0].length) {
-          continue;
-        }
-
-        if(!visitedTiles[newX][newY] && map[newX][newY]) {
+        // Check if the new coordinates are in the correct range and if they identify a land tile, explore it
+        if (newX >= 0 && newX < map.length && newY >= 0 && newY < map[0].length && map[newX][newY]) {
           q.add(newX);
           q.add(newY);
         }
